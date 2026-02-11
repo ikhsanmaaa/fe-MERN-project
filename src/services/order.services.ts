b0ba6b68bffcac0a1b0c6df515e3c438851f6fee
ikhsanmaaa <ikhsanmaaa@gmail.com>
2026-02-11 22:21:37 +0700

    fix order services

--- src/services/order.services.ts
+++ src/services/order.services.ts
@@ -5,7 +5,7 @@ import { ICart } from "@/types/Ticket";
 const OrderServices = {
   createOrder: (payload: ICart) => instance.post(endpoint.ORDER, payload),
   updateStatusTransaction: (id: string, status: string) =>
-    instance.put(`${endpoint.ORDER}/${id}/${status}`, status),
+    instance.put(`${endpoint.ORDER}/${id}/${status}`),
   orderHistory: (params: string) =>
     instance.get(`${endpoint.ORDER}-history?${params}`),
   getOrderById: (id: string) => instance.get(`${endpoint.ORDER}/${id}`),
